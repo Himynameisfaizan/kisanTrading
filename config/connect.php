29b5f3cc6c52fd5724bd7f55706e9b6532caafbc
Himynameisfaizan <faizansaikh830@gmail.com>
2026-09-24 12:45:06 +0530

    making website ready

--- config/connect.php
+++ config/connect.php
@@ -13,8 +13,8 @@ if ($local) {
     $host = 'localhost';
     $username = 'root';
     $password = '';
-    $dbName = 'bhagirath';
-    $site = "http://localhost/office_php_project/bhagirath/";
+    $dbName = 'kisantrading';
+    $site = "http://localhost/office_php_project/kisantrading/";
 } else {
     $host = 'localhost';
     $username = 'u799879276_bhagirath_db';
